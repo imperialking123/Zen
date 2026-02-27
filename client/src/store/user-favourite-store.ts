d86efaeff115dbe2842ddf38522321606b7493e4
Jacob Messiah <jacobmessiah71@gmail.com>
2026-02-27 19:14:39 +0100

    created user-favourite-store.ts to hold state on gifs and emoji because user-chat-store is getting bigger

---
+++ client/src/store/user-favourite-store.ts
@@ -0,0 +1,39 @@
+import type { GifCategory } from "@/types";
+import type { GifData } from "@/types/schema"
+import { axiosInstance } from "@/utils";
+import i18next from "../../i18nextConfig";
+import { create } from "zustand"
+
+
+type UserFavouriteStoreTypes = {
+    gifCategories: GifCategory[]
+    favouriteGifs: Record<string, GifData>;
+    toggleFavourite: (gif: GifData) => void;
+    getGifCategories: () => void
+}
+
+const UserFavouriteStore = create<UserFavouriteStoreTypes>((set) => ({
+    favouriteGifs: {},
+    toggleFavourite: (gif) => {
+        set((state) => {
+            const next = { ...state.favouriteGifs }
+            next[gif.id] ? delete next[gif.id] : next[gif.id] = gif
+            return { favouriteGifs: next }
+        })
+        axiosInstance.patch("/favourites/gifs/toggle", {
+            gif
+        })
+    },
+
+    getGifCategories: async () => {
+        try {
+            const response = await axiosInstance.get<{ locale: string, tags: GifCategory[] }>(`/gif/categories/${i18next.language}`)
+            set({ gifCategories: response.data.tags })
+        } catch (error) {
+            console.log("Coudn't get Categories ")
+        }
+    },
+    gifCategories: [],
+}))
+
+export default UserFavouriteStore
